0c777decd510b2422f252d454180ce9392e62740
Nate Grigg <n8mob@nategrigg.com>
2024-01-04 16:11:31 -0700

    find menu file at runtime (from program argument)

--- console_magie.py
+++ console_magie.py
@@ -1,3 +1,8 @@
+import argparse
+
+import requests
+from urllib3.util import parse_url
+import os
 from fixed_width import FixedWidthEncoding
 from game import Game
 from judgments import FullJudgment, CharJudgment
@@ -273,4 +278,14 @@ def start_game(json_path):
 
 
 if __name__ == '__main__':
-    start_game('TestMenus/FullMenu.json')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('menu_location')
+    args = parser.parse_args()
+    parsed_url = parse_url(args.menu_location)
+    if parsed_url.scheme in ['http', 'https']:
+        response = requests.get(parsed_url)
+        start_game(response.json())
+    elif os.path.exists(parsed_url.path):
+        start_game(parsed_url.path)
+    else:
+        raise FileNotFoundError(f"Can't figure out how to open {parsed_url}")
