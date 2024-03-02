31d9f9fbd076b030382e48a3a6d12106db58bf12
Nate Grigg <raspberry4a@nategrigg.com>
2024-03-02 15:40:40 -0700

    [wip] taking too long need to figure out how to work in PyCharm or something either emulate the DFRobot, or install PyCharm on this Raspberry Pi

---
+++ dfrobot_test.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.expanduser('~/c/DFRobot_RGB1602_RaspberryPi/python'))
+import rgb1602
+import RPi.GPIO as GPIO
+import time
+
+if __name__ == '__main__':
+  lines = [
+      'one',
+      'two',
+      'three',
+      'four'
+  ]
+
+  prev_line = ''
+
+  lcd = rgb1602.RGB1602(16,2)
+  
+  for line in lines:
+    lcd.setCursor(0,0)
+    lcd.printout(prev_line)
+    lcd.setCursor(0,1)
+    lcd.printout(line)
+    prev_line = line
+    time.sleep(0.5)
+    
